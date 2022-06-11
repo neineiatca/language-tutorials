a4c776292042f21ad58c5f6491aabd2f680d1935
neineiatca <sen.an.ottawa@gmail.com>
2022-06-10 23:52:10 -0400

    implement division

--- 001-print-primes/README.md
+++ 001-print-primes/README.md
@@ -1,4 +1,10 @@
 **input**: <br />
 max <br />
+
 **output**: <br />
 array of prime in [1 - max] <br />
+
+**check**
+loop - for
+condition - if
+type casting
--- 001-print-primes/rust/printPrimes.rs
+++ 001-print-primes/rust/printPrimes.rs
@@ -1,8 +1,14 @@
 fn aaa() {
     for dividend in 1..10 {
         for dividor in 1..dividend {
+            let dividendAsFloat = dividend as f64;
+            let dividorAsFloat = dividor as f64;
             let aaa = dividend / dividor;
-            println!("{}", dividend, dividor, aaa);
+            let bbb = aaa as f64;
+            let aaaAsFloat = dividendAsFloat / dividorAsFloat;
+            if bbb == aaaAsFloat {
+                println!("no remaider {}, {}", dividend, dividor);
+            }
         }
     }
 }
